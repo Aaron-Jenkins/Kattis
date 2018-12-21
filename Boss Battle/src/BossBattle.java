c57a7ebb80b52dc5953c557c60388eb6f1ad699d
Aaron Jenkins <JENKA17@solent.ac.uk>
2018-12-21 13:12:50 +0000

    Solved Boss Battle

--- Boss Battle/src/BossBattle.java
+++ Boss Battle/src/BossBattle.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BossBattle {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int pillar = in.nextInt();
+
+        if (pillar <= 3) {
+            System.out.println(1);
+        } else {
+            System.out.println(pillar - 2);
+        }
+
+
+
+    }
 }
